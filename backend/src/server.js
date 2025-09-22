cf7d6beb540492bf52cd0b79333c59342011e451
Duc Nguyen <ducnguyen255@gmail.com>
2025-09-22 20:42:34 +0700

    dirname fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,13 +17,13 @@ app.use("/api/tasks", taskRoute);
 
 
 const PORT = process.env.PORT || 5000;
-// const __dirname = path.resolve();
+
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "dist")));
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "dist/index.html"));
+        res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
     });
 }
 
