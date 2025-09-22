f28bd52f3014bf03aff3e591dde61abca8661ccf
Duc Nguyen <ducnguyen255@gmail.com>
2025-09-22 18:15:07 +0700

    prepare deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,29 @@ const taskRoute = require("./routes/tasksRouters.js");
 const connectDB = require("./config/db.js");
 require("dotenv").config();
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 //Middlewares
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+if (process.env.NODE_ENV === "production") {
+    app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 app.use("/api/tasks", taskRoute);
 
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    })
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
