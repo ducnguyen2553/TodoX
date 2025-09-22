e82f3eb6ce3141290eea7dbe3d3419e5833530b0
Duc Nguyen <ducnguyen255@gmail.com>
2025-09-22 20:16:35 +0700

    run build

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,10 +20,10 @@ const PORT = process.env.PORT || 5000;
 // const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+        res.sendFile(path.join(__dirname, "dist/index.html"));
     });
 }
 
